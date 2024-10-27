b12a4fe1d12fc5dbc7d2e06b57b2d38302e1f895
JuanCruzC97 <juancruzcamacho97@gmail.com>
2024-10-26 21:26:21 -0300

    add comments

--- src/crowdstream/cv/data_helpers/stanzarized.py
+++ src/crowdstream/cv/data_helpers/stanzarized.py
@@ -3,7 +3,16 @@ import cv2
 
 def stanzarize_video(input_path: str, output_path: str, width: int=1280, height: int=720, fps: int=24) -> None:
     """
-    Función que usa archivos de input y output.
+    Lleva un video a una resolución de 1280x720 y lo guarda en el directorio de salida.
+
+    Args:
+        input_path (str): _description_
+        output_path (str): _description_
+        width (int, optional): _description_. Defaults to 1280.
+        height (int, optional): _description_. Defaults to 720.
+        fps (int, optional): _description_. Defaults to 24.
+        
+    #TODO Se puede agregar la opción de cambiar los fps pero debe mantener la duración original del video (por ahora no se usan los fps).
     """
 
     # Captura de video desde el archivo de entrada
@@ -44,3 +53,19 @@ def stanzarize_video(input_path: str, output_path: str, width: int=1280, height:
     out.release()
     cv2.destroyAllWindows()
     print("Video procesado y guardado en:", output_path)
+    
+    
+    
+
+    def stanzarize_raw_videos() -> None:
+        
+        videos = [
+            "data/raw/people_dancing_demo_0.mp4",
+            "data/raw/people_dancing_demo_1.mp4",
+            "data/raw/people_dancing_demo_2.mp4",
+            "data/raw/people_dancing_demo_3.mp4"
+        ]
+        
+        for video in videos:
+            output_video = video.replace("raw", "stanzarized").replace(".mp4", "_std.mp4")
+            stanzarize_video(video, output_video, 1280, 720)
