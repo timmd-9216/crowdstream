f475fd90c3b04dde324e309aac80a56e2a01d516
JuanCruzC97 <juancruzcamacho97@gmail.com>
2024-11-10 20:51:11 -0300

    update tests

--- tests/test_cv/test_signal_container.py
+++ tests/test_cv/test_signal_container.py
@@ -1,22 +1,22 @@
 import pytest
 
-from crowdstream.cv.signal.signal_container import (SignalContainer,
-                                                    _keypoint_converter)
+from crowdstream.cv.signal.pose_signal import (PoseSignalContainer,
+                                               _keypoint_converter)
 from crowdstream.cv.utils.keypoint import Keypoint
 
 
 def test_keypoint_converter_default():
-    assert _keypoint_converter([9, 10, 11]) == [9, 10, 11]
+    assert _keypoint_converter([9, 10, 11]) == [[9], [10], [11]]
     
 def test_keypoint_converter_keypoints():
-    assert _keypoint_converter([Keypoint(9), Keypoint(10), Keypoint(11)]) == [9, 10, 11]
+    assert _keypoint_converter([Keypoint(9), Keypoint(10), Keypoint(11)]) == [[9], [10], [11]]
     
 def test_keypoint_converter_mixed():
-    assert _keypoint_converter([9, Keypoint(10), 11]) == [9, 10, 11]
+    assert _keypoint_converter([9, Keypoint(10), 11]) == [[9], [10], [11]]
     
 def test_keypoint_converter_error():
     with pytest.raises(ValueError):
         _keypoint_converter([9, "a", 11])
 
 def test_keypoint_converter_none():
-    assert _keypoint_converter(None) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    assert _keypoint_converter(None) == [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13], [14], [15], [16]]
