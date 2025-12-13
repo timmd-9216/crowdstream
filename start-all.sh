7b8630da5ce21504d9f4f43551ecdbb96c736563
Hernán Ordiales <hordiales@gmail.com>
2025-12-13 00:35:40 -0300

    update

--- start-all.sh
+++
@@ -1,1 +0,0 @@
-python mixer.py --host 127.0.0.1 --port 57120 &
--- start-crowdstream.sh
+++ start-crowdstream.sh
@@ -1,2 +1,9 @@
+
+source src/audio-engine/venv/bin/activate
+
+
 python audio_server.py --port 57120 &
-python mixer.py --host 127.0.0.1 --port 57120 &
+sleep 5
+python mixer.py --preflight-only 
+#python mixer.py --host 127.0.0.1 --port 57120 &
+python mixer.py --host 0.0.0.0 --port 57120 &
