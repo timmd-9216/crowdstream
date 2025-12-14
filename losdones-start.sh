bd1bb59881ef3b7087d79551a7c527a36ba7be06
Hernán Ordiales <hordiales@gmail.com>
2025-12-14 12:48:34 -0300

    update

---
+++ losdones-start.sh
@@ -0,0 +1,8 @@
+./start-all-services.sh  --visualizer cosmic_skeleton_standalone --no-dashboard 
+./start-chrome.sh 8094 &
+
+
+#old
+#./start-all-services.sh  --visualizer cosmic_skeleton --no-dashboard 
+#./start-chrome.sh 8091 &
+
